77dd85727e4ae380390f3bcfc9c6a66926cfd910
n/a <n/a>
2023-08-29 00:18:43 +0700

    improve(module): `mediasave`
    Signed-off-by: n/a <n/a>

--- ProjectDark/modules/mediasave.py
+++ ProjectDark/modules/mediasave.py
@@ -1,32 +1,22 @@
-# Part of Dragon-Userbot - 2022
-# Kang by DarkPyro - 2023
-
 import os
 
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
+from config import BOTLOG_CHATID as log
 from .help import add_command_help
 
 
 @Client.on_message(filters.command(["wow", "wah"], "") & filters.me)
 async def scrape(client: Client, message: Message):
     rep = message.reply_to_message
-    cap = rep.caption or None
-    
-    if rep.photo:
-        copy = await client.download_media(rep)
-        await message.delete()
-        await client.send_photo("me", copy, cap)
-        os.remove(copy)
-
-    elif rep.video:
-        copy = await client.download_media(rep)
-        await message.delete()
-        await client.send_video("me", copy, cap)
-        os.remove(copy)
-        return
-
+    if rep:
+        if rep.photo or rep.video:
+            await message.delete()
+            mtype = "photo" if rep.photo else "video"
+            copy = await client.download_media(rep)
+            await getattr(client, f"send_{mtype}")(log, copy, rep.caption)
+            os.remove(copy)
 
 add_command_help(
   "mediasave",
