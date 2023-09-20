c26982ddee8359d441c98baf94de51ba9c5e742c
KangerOnly <117170541+Ling-ex@users.noreply.github.com>
2023-09-20 19:16:03 +0700

    hoo

--- ProjectDark/modules/alive.py
+++ ProjectDark/modules/alive.py
@@ -21,8 +21,9 @@ photo = "https://telegra.ph//file/7310307cc29b4983c45d8.mp4"
 
 @Client.on_message(filters.command("alive", cmd) & filters.me)
 async def alive(client: Client, message: Message):
-    #msg = await edit_or_reply(message, "...")
+    msg = await edit_or_reply(message, "...")
     await asyncio.sleep(1)
+    send_mdia = client.send_video if alive_logo.endswith(".mp4") else client.send_photo
     uptime = await get_readable_time((time.time() - StartTime))
     logs = gvarstatus("BOTLOG_CHATID")
     logs = "Default" if logs == "me" else "Group"
@@ -42,7 +43,7 @@ Logs ID = {logs}
 Started since {uptime} ago.
 """)
     await asyncio.gather(
-                message.delete(),
-                client.send_photo(chat_id=message.chat.id, photo=photo, caption=alive_msg
-                                 ),
+                msg.delete(),
+                send_mdia(chat_id=message.chat.id, photo=photo, caption=alive_msg
+                ),
     )
