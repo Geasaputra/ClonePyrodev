df23661348ee52ab29379411e4808069f105780d
tr4ce <141042999+tracemoepy@users.noreply.github.com>
2023-09-20 18:52:36 +0700

    xixi

--- ProjectDark/modules/alive.py
+++ ProjectDark/modules/alive.py
@@ -17,7 +17,7 @@ from ProjectDark.helpers.basic import edit_or_reply
 from ProjectDark.utils import get_readable_time
 
 
-photo = "https://telegra.ph/file/36b93f50c3abe51d4f9f2.jpg"
+photo = "https://telegra.ph//file/c0b5e27763fa5a9f70bd1.jpg"
 
 @Client.on_message(filters.command("alive", cmd) & filters.me)
 async def alive(client: Client, message: Message):
